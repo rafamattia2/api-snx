9578b10c30c6f0fed9db99b8346126371391c974
rafamattia2 <rafaeldemattia.rm@gmail.com>
2024-11-12 20:50:31 -0300

    🐛 fix: removed /register from user creation route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,7 +5,7 @@ import { authMiddleware } from '../app/middlewares/authMiddleware.js';
 const router = express.Router();
 const userController = new UserController();
 
-router.post('/users/register', userController.registerUser);
+router.post('/users', userController.registerUser);
 router.post('/users/login', userController.loginUser);
 router.get('/users/:userId', authMiddleware, userController.getUser);
 
